981db7e14e8e75d45e0749dcdd2af22970e3d856
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-20 01:47:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,8 @@
-myArray = ["Dad", "Mom", "Brother", "Sister"]
-
 def using_push (colors_in_the_rainbow, next_color)
   next_color = "violet"
   colors_in_the_rainbow.push next_color
+end
+
+def using_unshift (array, string)
+  array.unshift string
 end
