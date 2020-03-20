06d028b408abaa3a3cc020edcf7f899c5364413c
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-20 01:46:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 myArray = ["Dad", "Mom", "Brother", "Sister"]
 
 def using_push (colors_in_the_rainbow, next_color)
-  next_color = "Red"
+  next_color = "violet"
   colors_in_the_rainbow.push next_color
 end
